ad68771e0e8250b19d5a4cc924dece026f60a063
fickry7 <fickry7@gmail.com>
2011-09-25 22:12:04 +0000

    masukin, list, sama delete sudah

--- THR/THR-war/src/java/model/Staff.java
+++ THR/THR-war/src/java/model/Staff.java
@@ -1,5 +1,13 @@
 package model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import util.Database;
+import util.EmailHandler;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -58,8 +66,8 @@ public class Staff {
         this.password = password;
     }
 
-    public Privilege getPrevilage() {
-        return previlage;
+    public String getPrevilage() {
+        return previlage.getValue();
     }
 
     public void setPrevilage(Privilege previlage) {
@@ -74,5 +82,95 @@ public class Staff {
         this.username = username;
     }
     
+    public Staff getStaff(int ids){
+        Database db = new Database();
+        ResultSet rs;
+        Staff s = new Staff();
+        String Sql;
+        Sql="SELECT * FROM customer WHERE idc='"+ids+"'";
+        db.setConnection();
+        rs = db.executingQuery(Sql);
+        try {
+            while(rs.next()){
+                s.setIds(rs.getInt("ids"));
+                s.setUsername(rs.getString("username"));
+                s.setFirst_name(rs.getString("first_name"));
+                s.setLast_name(rs.getString("last_name"));
+                s.setEmail(rs.getString("email"));
+                s.setPrevilage(new Privilege(rs.getString("previlege")));
+                s.setPassword(rs.getString("password"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Customer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return s;
+    }
     
+    public String addStaff()
+    {
+        Database db = new Database();
+        EmailHandler em = new EmailHandler();
+        String sql = null;
+        try{
+            String passmd5 = em.getStringMD5(this.getPassword());
+            sql="INSERT INTO staff (username,first_name,last_name,email,previlege,password) VALUES "
+                    + "('"+this.getUsername()+"','"+this.getFirst_name()+"','"+this.getLast_name()+"'"
+                    + ",'"+this.getEmail()+"','"
+                    + this.getPrevilage() +"','"+this.getPassword()+"')";
+            db.setConnection();
+            db.updatingQuery(sql);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        finally{
+            db.unsetConnection();
+        }
+        return sql;
+    }
+    
+    public String deleteStaff(String ids)
+    {
+        Database db = new Database();
+        String sql = null;
+        try{
+            sql="DELETE FROM staff WHERE ids="+ids;
+            db.setConnection();
+            db.updatingQuery(sql);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        finally{
+            db.unsetConnection();
+        }
+        return sql;
+    }
+    
+    public ArrayList<Staff> getallStaff(){
+        Database db = new Database();
+        ResultSet rs;
+        ArrayList<Staff> temp =new ArrayList<Staff>();
+        String sql;
+        try{
+            sql="SELECT * FROM staff";
+            db.setConnection();
+            rs = db.executingQuery(sql) ;
+            while (rs.next()) {
+                Staff s = new Staff();
+                s.setIds(rs.getInt("ids"));
+                s.setUsername(rs.getString("username"));
+                s.setFirst_name(rs.getString("first_name"));
+                s.setLast_name(rs.getString("last_name"));
+                s.setEmail(rs.getString("email"));
+                s.setPrevilage(new Privilege(rs.getString("previlege")));
+                s.setPassword(rs.getString("password"));
+                temp.add(s);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        finally{
+            db.unsetConnection();
+        }
+        return temp ;
+    }
 }
