04ecc35b867a7f275bd16edcaa124956ea95f624
sidiksoleman <sidiksoleman@gmail.com>
2011-10-23 21:03:06 +0000

    stable 1....

--- THR/THR-war/src/java/servlet/editProfilController.java
+++ THR/THR-war/src/java/servlet/editProfilController.java
@@ -18,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import model.Customer;
 import model.DateFormater;
+import model.Staff;
 import util.Database;
 import util.EmailHandler;
 
@@ -40,80 +41,53 @@ public class editProfilController extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
-            // TODO output your page here
-            String first_name = new String();
-            String last_name = new String();
-            String address = new String();
-            String phone = new String();
-            String place_of_birth = new String();
-            String date_of_birth = new String();
-            String password = new String();
-            String password_confirm = new String();
-            String idc = new String();
-            int id;
+            if (request.getParameter("user").equals("0")) {
+                // TODO output your page here
+                String first_name = new String();
+                String last_name = new String();
+                String address = new String();
+                String phone = new String();
+                String place_of_birth = new String();
+                String date_of_birth = new String();
+                String password = new String();
+                String password_confirm = new String();
+                String idc = new String();
+                int id;
 
-            String sql = new String();
+                String sql = new String();
 
-            first_name = request.getParameter("first_name");
-            last_name = request.getParameter("last_name");
-            address = request.getParameter("address");
-            phone = request.getParameter("phone");
-            place_of_birth = request.getParameter("place_of_birth");
-            date_of_birth = request.getParameter("date_of_birth");
-            password = request.getParameter("password");
-            password_confirm = request.getParameter("password_confirm");
-            idc = request.getParameter("idc");
-            id = Integer.parseInt(idc);
+                first_name = request.getParameter("first_name");
+                last_name = request.getParameter("last_name");
+                address = request.getParameter("address");
+                phone = request.getParameter("phone");
+                place_of_birth = request.getParameter("place_of_birth");
+                date_of_birth = request.getParameter("date_of_birth");
+                if (request.getParameter("managed") == null) {
+                    password = request.getParameter("password");
+                    password_confirm = request.getParameter("password_confirm");
+                }
+                idc = request.getParameter("idc");
+                id = Integer.parseInt(idc);
 
-            HttpSession session = request.getSession();
-            if (session.getAttribute("user") == null) {
-                response.sendRedirect("index.jsp");
-            } else {
-                Customer cust = (Customer) session.getAttribute("user");
-
-                if (password.equals("")) {
-                    cust.setFirst_name(first_name);
-                    cust.setLast_name(last_name);
-                    cust.setAddress(address);
-                    cust.setPhone(phone);
-                    cust.setPlace_of_birth(place_of_birth);
-                    cust.setDate_of_birth(DateFormater.getDateFromViewFormat(date_of_birth));
-                    sql = "UPDATE customer SET first_name='" + first_name + "', last_name='" + last_name + "'";
-                    if (!address.equals("")) {
-                        sql = sql.concat(", address='" + address + "'");
-                    } else {
-                        sql = sql.concat(", address=Null");
-                    }
-                    if (!phone.equals("")) {
-                        sql = sql.concat(", phone='" + phone + "'");
-                    } else {
-                        sql = sql.concat(", phone=Null");
-                    }
-                    if (!place_of_birth.equals("")) {
-                        sql = sql.concat(", place_of_birth='" + place_of_birth + "'");
-                    } else {
-                        sql = sql.concat(", place_of_birth=Null");
-                    }
-                    if (!date_of_birth.equals("")) {
-                        sql = sql.concat(", date_of_birth='" + DateFormater.formatDateToDBFormat(date_of_birth) + "'");
-                    } else {
-                        sql = sql.concat(", date_of_birth=Null");
-                    }
-                    sql = sql.concat(" WHERE idc='" + id + "'");
-                    System.out.println(sql);
-                    Database.setConnection();
-                    Database.updatingQuery(sql);
-                    response.sendRedirect("editProfile/editProfilPage.jsp?success=1");
+                HttpSession session = request.getSession();
+                if (session.getAttribute("user") == null) {
+                    response.sendRedirect("index.jsp");
                 } else {
-                    if (password.equals(password_confirm)) {
-                        EmailHandler e = new EmailHandler();
-                        cust.setFirst_name(first_name);
-                        cust.setLast_name(last_name);
-                        cust.setAddress(address);
-                        cust.setPhone(phone);
-                        cust.setPlace_of_birth(place_of_birth);
-                        cust.setDate_of_birth(DateFormater.getDateFromViewFormat(date_of_birth));
-                        cust.setPassword(password);
+                    Customer cust = new Customer();
+                    if (request.getParameter("managed") == null) {
+                        cust = (Customer) session.getAttribute("user");
+                    } else {
+                        cust = cust.getCustomer(idc);
+                    }
+                    if (password.equals("")) {
+                        if (request.getParameter("managed") == null) {
+                            cust.setFirst_name(first_name);
+                            cust.setLast_name(last_name);
+                            cust.setAddress(address);
+                            cust.setPhone(phone);
+                            cust.setPlace_of_birth(place_of_birth);
+                            cust.setDate_of_birth(DateFormater.getDateFromViewFormat(date_of_birth));
+                        }
                         sql = "UPDATE customer SET first_name='" + first_name + "', last_name='" + last_name + "'";
                         if (!address.equals("")) {
                             sql = sql.concat(", address='" + address + "'");
@@ -135,24 +109,162 @@ public class editProfilController extends HttpServlet {
                         } else {
                             sql = sql.concat(", date_of_birth=Null");
                         }
-                        try {
-                            password = e.getStringMD5(password);
-                        } catch (NoSuchAlgorithmException ex) {
-                            Logger.getLogger(editProfilController.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                        if (!password.equals("")) {
-                            sql = sql.concat(", password='" + password + "'");
-                        }
                         sql = sql.concat(" WHERE idc='" + id + "'");
+                        System.out.println(sql);
                         Database.setConnection();
                         Database.updatingQuery(sql);
-                        response.sendRedirect("editProfile/editProfilPage.jsp?success=1");
+                        if (request.getParameter("managed") != null) {
+                            if (request.getParameter("managed").equals("staff")) {
+                                response.sendRedirect("usermanagement/ListStaffView.jsp");
+                            } else if (request.getParameter("managed").equals("customer")) {
+                                response.sendRedirect("usermanagement/ListCustomerView.jsp");
+                            }
+                        } else {
+                            response.sendRedirect("editProfile/editProfilPage.jsp?success=1");
+                        }
                     } else {
-                        response.sendRedirect("editProfile/editProfilPage.jsp?success=0");
+                        if (password.equals(password_confirm)) {
+                            EmailHandler e = new EmailHandler();
+                            cust.setFirst_name(first_name);
+                            cust.setLast_name(last_name);
+                            cust.setAddress(address);
+                            cust.setPhone(phone);
+                            cust.setPlace_of_birth(place_of_birth);
+                            cust.setDate_of_birth(DateFormater.getDateFromViewFormat(date_of_birth));
+                            cust.setPassword(password);
+                            sql = "UPDATE customer SET first_name='" + first_name + "', last_name='" + last_name + "'";
+                            if (!address.equals("")) {
+                                sql = sql.concat(", address='" + address + "'");
+                            } else {
+                                sql = sql.concat(", address=Null");
+                            }
+                            if (!phone.equals("")) {
+                                sql = sql.concat(", phone='" + phone + "'");
+                            } else {
+                                sql = sql.concat(", phone=Null");
+                            }
+                            if (!place_of_birth.equals("")) {
+                                sql = sql.concat(", place_of_birth='" + place_of_birth + "'");
+                            } else {
+                                sql = sql.concat(", place_of_birth=Null");
+                            }
+                            if (!date_of_birth.equals("")) {
+                                sql = sql.concat(", date_of_birth='" + DateFormater.formatDateToDBFormat(date_of_birth) + "'");
+                            } else {
+                                sql = sql.concat(", date_of_birth=Null");
+                            }
+                            try {
+                                password = e.getStringMD5(password);
+                            } catch (NoSuchAlgorithmException ex) {
+                                Logger.getLogger(editProfilController.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                            if (!password.equals("")) {
+                                sql = sql.concat(", password='" + password + "'");
+                            }
+                            sql = sql.concat(" WHERE idc='" + id + "'");
+                            Database.setConnection();
+                            Database.updatingQuery(sql);
+                            if (request.getParameter("managed") != null) {
+                                if (request.getParameter("managed").equals("staff")) {
+                                    response.sendRedirect("usermanagement/ListStaffView.jsp");
+                                } else if (request.getParameter("managed").equals("customer")) {
+                                    response.sendRedirect("usermanagement/ListCustomerView.jsp");
+                                }
+                            } else {
+                                response.sendRedirect("editProfile/editProfilPage.jsp?success=1");
+                            }
+                        } else {
+                            response.sendRedirect("editProfile/editProfilPage.jsp?success=0");
+                        }
+                    }
+                }
+            } else {
+                // TODO output your page here
+                String first_name = new String();
+                String last_name = new String();
+                String password = new String();
+                String password_confirm = new String();
+                String idc = new String();
+                int id;
+
+                String sql = new String();
+
+                first_name = request.getParameter("first_name");
+                last_name = request.getParameter("last_name");
+                if (request.getParameter("managed") == null) {
+                    password = request.getParameter("password");
+                    password_confirm = request.getParameter("password_confirm");
+                }
+                idc = request.getParameter("idc");
+                id = Integer.parseInt(idc);
+
+                HttpSession session = request.getSession();
+                if (session.getAttribute("user") == null) {
+                    response.sendRedirect("index.jsp");
+                } else {
+                    Staff cust = new Staff();
+                    if (request.getParameter("managed") == null) {
+                        cust = (Staff) session.getAttribute("user");
+                    } else {
+                        cust = cust.getStaff(idc);
+                    }
+
+                    if (password.equals("")) {
+                        if (request.getParameter("managed") == null) {
+                            cust.setFirst_name(first_name);
+                            cust.setLast_name(last_name);
+                        }
+                        sql = "UPDATE staff SET first_name='" + first_name + "', last_name='" + last_name + "'";
+                        if (request.getParameter("managed") != null) {
+                            if (request.getParameter("managed").equals("staff")) {
+                                sql = sql.concat(", previlege='" + request.getParameter("previlege")+"'");
+                            }
+                        }
+                        sql = sql.concat(" WHERE ids='" + id + "'");
+                        Database.setConnection();
+                        Database.updatingQuery(sql);
+                        if (request.getParameter("managed") != null) {
+                            if (request.getParameter("managed").equals("staff")) {
+                                response.sendRedirect("usermanagement/ListStaffView.jsp");
+                            } else if (request.getParameter("managed").equals("customer")) {
+                                response.sendRedirect("usermanagement/ListCustomerView.jsp");
+                            }
+                        } else {
+                            response.sendRedirect("editProfile/editProfilPage.jsp?success=1");
+                        }
+                    } else {
+                        if (password.equals(password_confirm)) {
+                            EmailHandler e = new EmailHandler();
+                            cust.setFirst_name(first_name);
+                            cust.setLast_name(last_name);
+                            cust.setPassword(password);
+                            sql = "UPDATE staff SET first_name='" + first_name + "', last_name='" + last_name + "'";
+                            try {
+                                password = e.getStringMD5(password);
+                            } catch (NoSuchAlgorithmException ex) {
+                                Logger.getLogger(editProfilController.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                            if (!password.equals("")) {
+                                sql = sql.concat(", password='" + password + "'");
+                            }
+                            sql = sql.concat(" WHERE ids='" + id + "'");
+                            Database.setConnection();
+                            Database.updatingQuery(sql);
+                            if (request.getParameter("managed") != null) {
+                                if (request.getParameter("managed").equals("staff")) {
+                                    response.sendRedirect("usermanagement/ListStaffView.jsp");
+                                } else if (request.getParameter("managed").equals("customer")) {
+                                    response.sendRedirect("usermanagement/ListCustomerView.jsp");
+                                }
+                            } else {
+                                response.sendRedirect("editProfile/editProfilPage.jsp?success=1");
+                            }
+                        } else {
+                            response.sendRedirect("editProfile/editProfilPage.jsp?success=0");
+                        }
                     }
                 }
             }
-
         } finally {
             out.close();
         }
