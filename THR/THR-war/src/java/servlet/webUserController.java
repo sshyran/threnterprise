ea5057495e609074c23c3bd5b9863ac0fea91a71
sidiksoleman <sidiksoleman@gmail.com>
2011-11-12 11:58:18 +0000

    Removing unused servlet ...

--- THR/THR-war/src/java/servlet/webUserController.java
+++ THR/THR-war/src/java/servlet/webUserController.java
@@ -11,9 +11,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import model.Customer;
-import model.Staff;
 import model.Webservice;
+import util.Database;
 
 /**
  *
@@ -34,52 +33,108 @@ public class webUserController extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
-            if (request.getParameter("manage").equals("staff")) {
-                if (request.getParameter("action") != null) {
-                    if (request.getParameter("action").equals("delete")) {
-                        Staff s = new Staff();
-                        s.deleteStaff(request.getParameter("id"));
-                        response.sendRedirect("usermanagement/ListStaffView.jsp?success=1");
-                    } else if (request.getParameter("action").equals("edit")) {
-                        response.sendRedirect("editProfile/editProfilPage.jsp?manage=edit&target=staff&id=" + request.getParameter("id"));
-                    } else if(request.getParameter("action").equals("add")){
-                        response.sendRedirect("editProfile/editProfilPage.jsp?manage=add&target=staff");
+            if (request.getParameter("manage").equals("ws")) {
+                if (request.getParameter("save") == null) {
+                    if (request.getParameter("action") != null) {
+                        if (request.getParameter("action").equals("delete")) {
+                            Webservice ws = new Webservice();
+                            ws.deleteWebService(request.getParameter("id"));
+                            response.sendRedirect("manageWS/webservicePage.jsp?success=1");
+                        } else if (request.getParameter("action").equals("edit")) {
+                            response.sendRedirect("manageWS/editWebservicePage.jsp?manage=edit&target=ws&id=" + request.getParameter("id"));
+                        } else if (request.getParameter("action").equals("add")) {
+                            response.sendRedirect("manageWS/editWebservicePage.jsp?manage=add&target=ws&success=1");
+                        }
+                    } else {
+                        response.sendRedirect("manageWS/webservicePage.jsp");
                     }
                 } else {
-                    response.sendRedirect("usermanagement/ListStaffView.jsp");
-                }
-            } else if (request.getParameter("manage").equals("kustomer")) {
-                if (request.getParameter("action") != null) {
-                    if (request.getParameter("action").equals("delete")) {
-                        Customer s = new Customer();
-                        s.deleteCustomer(request.getParameter("id"));
-                        response.sendRedirect("usermanagement/ListCustomerView.jsp?success=1");
-                    } else if (request.getParameter("action").equals("edit")) {
-                        response.sendRedirect("editProfile/editProfilPage.jsp?manage=edit&target=customer&id=" + request.getParameter("id"));
-                    }else if(request.getParameter("action").equals("add")){
-                        response.sendRedirect("editProfile/editProfilPage.jsp?manage=add&target=customer");
+                    boolean edit = true;
+                    if (request.getParameter("action") != null) {
+                        if (request.getParameter("action").equals("add")) {
+                            edit = false;
+                        }
                     }
-                } else {
-                    response.sendRedirect("usermanagement/ListCustomerView.jsp");
+
+                    String nama = new String();
+                    String jenis = new String();
+                    String server = new String();
+                    String port = new String();
+                    String username = new String();
+                    String password = new String();
+                    String ids = new String();
+                    String sql = new String();
+                    int id = 0;
+
+                    nama = request.getParameter("nama");
+                    jenis = request.getParameter("jenis");
+                    server = request.getParameter("server");
+                    port = request.getParameter("port");
+                    username = request.getParameter("username");
+                    password = request.getParameter("password");
+                    if (edit) {
+                        ids = request.getParameter("idws");
+                        id = Integer.parseInt(ids);
+                        if (nama.equals("") || server.equals("")) {
+                            response.sendRedirect("manageWS/editWebservicePage.jsp?manage=edit&target=ws&id=" + ids + "&success=0");
+                        } else {
+                            sql = "UPDATE webservice SET nama='" + nama + "',jenis='" + jenis + "',server='" + server + "'";
+                            if (!port.equals("")) {
+                                sql = sql.concat(",port='" + port + "'");
+                            } else {
+                                sql = sql.concat(",port=Null");
+                            }
+                            if (!username.equals("")) {
+                                sql = sql.concat(",username='" + username + "'");
+                            } else {
+                                sql = sql.concat(",username=Null");
+                            }
+                            if (!password.equals("")) {
+                                sql = sql.concat(",password='" + password + "'");
+                            } else {
+                                sql = sql.concat(",password=Null");
+                            }
+                            sql = sql.concat(" WHERE id='" + ids + "'");
+
+                            out.print(sql);
+                            Database.setConnection();
+                            Database.updatingQuery(sql);
+                            response.sendRedirect("manageWS/webservicePage.jsp?success=2");
+                        }
+                    } else {
+                        if (nama.equals("") || server.equals("")) {
+                            response.sendRedirect("manageWS/editWebservicePage.jsp?manage=edit&target=ws&success=0");
+                        } else {
+                            sql = "INSERT INTO webservice (nama,jenis,server,port,username,password) VALUES ('" + nama + "','" + jenis + "','" + server + "'";
+                            if (!port.equals("")) {
+                                sql = sql.concat(",'" + port + "'");
+                            } else {
+                                sql = sql.concat(",Null");
+                            }
+                            if (!username.equals("")) {
+                                sql = sql.concat(",'" + username + "'");
+                            } else {
+                                sql = sql.concat(",Null");
+                            }
+                            if (!password.equals("")) {
+                                sql = sql.concat(",'" + password + "'");
+                            } else {
+                                sql = sql.concat(",Null");
+                            }
+                            sql = sql.concat(")");
+                            out.print(sql);
+                            Database.setConnection();
+                            Database.updatingQuery(sql);
+
+                            response.sendRedirect("manageWS/webservicePage.jsp?success=3");
+                        }
+
+                    }
                 }
+            } else {
+                response.sendRedirect("manageWS/webservicePage.jsp");
             }
-            else if (request.getParameter("manage").equals("ws")){
-                if(request.getParameter("action")!=null){
-                    if(request.getParameter("action").equals("delete")){
-                        Webservice ws = new Webservice();
-                        ws.deleteWebService(request.getParameter("id"));
-                        response.sendRedirect("manageWS/webservicePage.jsp?success=1");
-                    }
-                    else if(request.getParameter("action").equals("edit")){
-                        response.sendRedirect("manageWS/editWebservicePage.jsp?manage=edit&target=ws&id="+request.getParameter("id"));
-                    }
-                    else if(request.getParameter("action").equals("add")){
-                        response.sendRedirect("manageWS/editWebservicePage.jsp?manage=add&target=ws&success=1");
-                    }
-                } else {
-                    response.sendRedirect("manageWS/webservicePage.jsp");
-                }
-            }
+
 
         } finally {
             out.close();
